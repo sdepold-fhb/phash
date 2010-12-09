c5ab6c2208e6ee197b6a80284166bd7c65bb80b6
Sascha Depold <sascha@depold.com>
2010-12-09 17:18:30 +0100

    removed demo code from lib

--- ruby/phash.rb
+++ ruby/phash.rb
@@ -55,12 +55,3 @@ class PHash
     { :value => value, :is_similiar => is_similiar }
   end
 end
-
-file1="/Users/prototype/Pictures/Wallpapers/11002003953.jpg"
-file2="/Users/prototype/Pictures/Wallpapers/11002013080.jpg"
-
-p_hash = PHash.new(file1, file2, {:proxy => "proxy.fh-brandenburg.de:3128"})
-
-puts p_hash.compare(PHash::ALGORITHMS[0]).inspect
-puts p_hash.compare(PHash::ALGORITHMS[1]).inspect
-puts p_hash.compare(PHash::ALGORITHMS[2]).inspect
