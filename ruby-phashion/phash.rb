76f1a96a842727aeb9cbba1049be3fb30b296ed2
Sascha Depold <sascha@depold.com>
2010-12-10 14:08:56 +0100

    added image upload index

--- ruby-phashion/phash.rb
+++ ruby-phashion/phash.rb
@@ -2,8 +2,22 @@ require "rubygems"
 require "sinatra"
 require "sinatra/reloader" if development?
 require "phashion"
-require "haml"
+require "erb"
+require "RMagick"
+include Magick
 
 get "/" do
-  haml :index
+  erb :index
+end
+
+post "/upload" do
+  upload_path = File.expand_path("./uploads/image_#{Time.now.to_i}")
+  FileUtils.mv(params[:image][:tempfile].path, upload_path)
+  redirect "/analyze/#{File.basename(upload_path)}"
+end
+
+get "/analyze/:filename" do
+  path = File.expand_path("./uploads/#{params[:filename]}")
+  mime_type = ImageList.new(path).mime_type
+  send_file path, :type => mime_type
 end
